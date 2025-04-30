e1857dacb87cd1019345d89aa94dec42d7b7ade4
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-04-30 17:56:53 +0300

    Integrate NestjsFormDataModule into AuthModule to parse form-data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,39 @@
-import { Controller, Post, Param, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { ValidationPipe } from '../utils/validation-pipe';
+import { RegisterDTO } from './dto/register-dto';
+import { VerifyEmailDTO } from './dto/verify-email-dto';
+import { LoginDTO } from './dto/login-dto';
+import { FormDataRequest } from 'nestjs-form-data';
 
 @Controller('auth')
 export class AuthController {
   @Post('/register')
-  register(@Param() param) {
-    console.log('Param obj is', param);
+  @FormDataRequest()
+  register(
+    @Body(new ValidationPipe())
+    body: RegisterDTO,
+  ) {
     return {
       message: 'user register successfully',
+      body,
     };
   }
   @Post('/verify-email')
-  verify(@Body() body) {
+  @FormDataRequest()
+  verify(@Body(new ValidationPipe()) body: VerifyEmailDTO) {
     console.log('Body obj,', body);
-    return 'Email verified';
+    return {
+      message: 'email verified',
+      body,
+    };
   }
 
   @Post('/login')
-  login() {
-    return 'login successfully';
+  @FormDataRequest()
+  login(@Body(new ValidationPipe()) body: LoginDTO) {
+    return {
+      message: 'login successfully',
+      body,
+    };
   }
 }
