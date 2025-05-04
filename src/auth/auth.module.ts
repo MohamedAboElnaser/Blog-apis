700dd960e8a36f90d36124b787b7e528edc22140
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-04 16:40:29 +0300

    Solve circular dependency between UserModule and AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { UserModule } from 'src/user/user.module';
+import { PasswordService } from './password.service';
 
 @Module({
-  imports: [NestjsFormDataModule, UserModule],
+  imports: [NestjsFormDataModule, forwardRef(() => UserModule)],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, PasswordService],
+  exports: [PasswordService],
 })
 export class AuthModule {}
