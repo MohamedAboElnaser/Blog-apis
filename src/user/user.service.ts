eb2b771d39b3190d6154ad48b4c3b039cfd80e98
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-04 16:37:27 +0300

    Implement add method at UserService class

---
+++ src/user/user.service.ts
@@ -0,0 +1,32 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterDTO } from 'src/auth/dto/register-dto';
+import { User } from './entities/user.entity';
+import { QueryFailedError, Repository } from 'typeorm';
+import { PasswordService } from 'src/auth/password.service';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    private readonly passwordService: PasswordService,
+  ) {}
+  async add(data: RegisterDTO) {
+    try {
+      data.password = await this.passwordService.hashPassword(data.password);
+      const userInstance = this.usersRepository.create(data);
+      const user = await this.usersRepository.save(userInstance);
+      const { password, ...result } = user;
+      return result;
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new ConflictException('Email already used');
+      }
+      throw new InternalServerErrorException('An unexpected error occurred');
+    }
+  }
+}
