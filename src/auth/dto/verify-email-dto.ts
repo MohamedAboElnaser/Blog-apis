b08a6a2a768b81a97d08aaec88a0f75477cb302d
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-04 22:29:06 +0300

    Enhance VerifyEmailDTO
    - Make sure that the email is not empty field

--- src/auth/dto/verify-email-dto.ts
+++ src/auth/dto/verify-email-dto.ts
@@ -1,6 +1,7 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class VerifyEmailDTO {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
