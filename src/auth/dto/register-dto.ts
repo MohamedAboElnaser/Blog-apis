767af5fa608e9b1964d470e17cfdfc2403d9b7fa
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-04 17:43:09 +0300

    Enhance RegisterDTO class

--- src/auth/dto/register-dto.ts
+++ src/auth/dto/register-dto.ts
@@ -2,13 +2,14 @@ import { IsEmail, Length, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class RegisterDTO {
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @Length(8, 20)
   password: string;
 
   @IsNotEmpty()
-  @Length(3, 10, { message: 'firstName must be between 3 and 10 characters' })
+  @Length(3, 10)
   firstName: string;
 
   @IsOptional()
