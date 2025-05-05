13388766b13348254e6f1824afe46f95d8a7951e
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-05 18:57:38 +0300

    feat: add VerifyEmailDTO with email and OTP validation

--- src/auth/dto/verify-email-dto.ts
+++ src/auth/dto/verify-email-dto.ts
@@ -1,7 +1,13 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, Min, Max } from 'class-validator';
 
 export class VerifyEmailDTO {
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @IsNumber()
+  @Min(100000)
+  @Max(999999)
+  @IsNotEmpty()
+  otp: number;
 }
