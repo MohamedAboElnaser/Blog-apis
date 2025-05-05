27efb2d105ebdd5439f0f48d3f9db31992b265e1
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-05 18:54:49 +0300

    feat: add OTP entity with email and code fields

---
+++ src/otp/entities/otp.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Otp {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  code: number;
+}
