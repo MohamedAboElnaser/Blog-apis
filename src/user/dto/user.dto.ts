9ba651af49e6bbb057cf45c11cba333de60e9d07
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-13 20:00:14 +0300

    docs : add UserDto

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserDto {
+  @ApiProperty({ example: 1 })
+  id: number;
+
+  @ApiProperty({ example: 'username@example.com' })
+  email: string;
+
+  @ApiProperty({ example: 'John' })
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe' })
+  lastName: string;
+
+  @ApiProperty({ example: false })
+  isVerified: boolean;
+
+  @ApiProperty({ example: '2025-05-13T12:00:00Z' })
+  createdAt: Date;
+
+  @ApiProperty({ example: '2025-05-13T12:00:00Z' })
+  updatedAt: Date;
+}
