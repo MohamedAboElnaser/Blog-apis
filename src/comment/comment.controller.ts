25c0fd6a03797452fa58c22bfe21c23f19a3e196
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-16 18:57:18 +0300

    docs(comment): add swagger docs for create comment endpoint

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -10,6 +10,7 @@ import {
   ValidationPipe,
   UsePipes,
   Get,
+  HttpStatus,
 } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
@@ -17,7 +18,17 @@ import { UpdateCommentDto } from './dto/update-comment.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { RequestWithUser } from 'src/auth/types/request.type';
 import { FormDataRequest } from 'nestjs-form-data';
-
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { CommentResponseDto } from './dto/comment-response-dto';
+@ApiTags('Comments')
 @Controller('blogs/:blogId/comments')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
@@ -26,12 +37,42 @@ export class CommentController {
   @FormDataRequest({})
   @UseGuards(AuthGuard)
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  create(
+  @ApiOperation({ summary: 'Add comment to a public Blog' })
+  @ApiBearerAuth()
+  @ApiConsumes('multipart/form-data', 'application/json')
+  @ApiParam({
+    name: 'blogId',
+    type: 'number',
+    description: 'ID of the blog to comment on',
+  })
+  @ApiBody({ type: CreateCommentDto })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'Comment created successfully',
+    type: CommentResponseDto,
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - User not authenticated',
+  })
+  @ApiResponse({
+    status: HttpStatus.FORBIDDEN,
+    description: 'Forbidden - Blog is not public',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not Found - Blog does not exist',
+  })
+  @ApiResponse({
+    status: HttpStatus.BAD_REQUEST,
+    description: 'Bad Request - Invalid input data',
+  })
+  async create(
     @Body() createCommentDto: CreateCommentDto,
     @Param('blogId') blogId: number,
     @Request() req: RequestWithUser,
   ) {
-    return this.commentService.create({
+    return await this.commentService.create({
       body: createCommentDto.body,
       blogId,
       authorId: req.user.sub,
--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateCommentDto {
+  @ApiProperty({
+    description: 'Content of the comment',
+    example: 'Awesome!',
+  })
   @IsNotEmpty()
   body: string;
 }
