0f94a3a8029718dd390334aa8d0d471aed833f70
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2025-05-17 22:38:11 +0300

    feat: add health check endpoint with Swagger documentation

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!-welcome to Nest-js';
+  getHello() {
+    return { message: 'Server is up and running...' };
   }
 }
